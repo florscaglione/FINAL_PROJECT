a0851ea0dc69983bf771f5d212240dea6907c513
SMUTIS <sandramutissanabria@gmail.com>
2021-10-01 18:38:58 +0000

    prueba

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -21,6 +21,8 @@ export const Demo = () => {
 								<span>Link to: {item.title}</span>
 							</Link>
 							{// Conditional render example
+
+							//holi
 							// Check to see if the background is orange, if so, display the message
 							item.background === "orange" ? (
 								<p style={{ color: item.initial }}>
