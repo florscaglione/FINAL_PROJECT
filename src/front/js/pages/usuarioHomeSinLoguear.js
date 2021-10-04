0ffd7557685513cbbe85cc340725a810d3b52b4e
Uxbel <beldetes@gmail.com>
2021-10-04 15:40:51 +0000

    vista usuario sin loguear v1

--- src/front/js/pages/usuarioHomeSinLoguear.js
+++ src/front/js/pages/usuarioHomeSinLoguear.js
@@ -1,82 +1,47 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import cardSelector from "../../img/paginaInicio/card-selector.png";
-import infoInicio from "../../img/paginaInicio/info-inicio.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => {
+export const HomeUsuarioSinLoguear = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col text-center mt-4">
-					<h1>Work From Home - WFH</h1>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col text-center">
-					<div className="card">
-						<img src={cardSelector} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Para empresas</h5>
-							<p className="card-text">
-								Accede para poder publicar una oferta de teletrabajo y los usuarios puedan aplicar.
-							</p>
-							<a className="btn btn-primary">Publicar oferta</a>
+					<h1>Home Usuario Sin Loguear</h1>
+					<form className="d-flex">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+					<div className="">
+						<div className="dropdown">
+							<button
+								className="btn btn-secondary dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton1"
+								data-bs-toggle="dropdown"
+								aria-expanded="false">
+								Filtro 1
+							</button>
+							<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+								<li>
+									<a className="dropdown-item">Action</a>
+								</li>
+								<li>
+									<a className="dropdown-item">Another action</a>
+								</li>
+								<li>
+									<a className="dropdown-item">Something else here</a>
+								</li>
+							</ul>
 						</div>
 					</div>
 				</div>
-				<div className="col text-center">
-					<div className="card">
-						<img src={cardSelector} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Para usuarios</h5>
-							<p className="card-text">
-								¿Estás buscando ofertas de teletrabajo? Consulta las mejores ofertas disponibles.
-							</p>
-							<a className="btn btn-primary">Ofertas de teletrabajo</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="row">
 				<div className="col text-center mt-4">
-					<h2>Nuestros números</h2>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col text-center">
-					<div className="card">
-						<img src={infoInicio} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">CV</h5>
-						</div>
-					</div>
-				</div>
-				<div className="col text-center">
-					<div className="card">
-						<img src={infoInicio} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Empresas</h5>
-						</div>
-					</div>
-				</div>
-				<div className="col text-center">
-					<div className="card">
-						<img src={infoInicio} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Ofertas</h5>
-						</div>
-					</div>
-				</div>
-				<div className="col text-center">
-					<div className="card">
-						<img src={infoInicio} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Ventajas</h5>
-						</div>
-					</div>
+					<p>Sidebar</p>
 				</div>
 			</div>
 		</div>
