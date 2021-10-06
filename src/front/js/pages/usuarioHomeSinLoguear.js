bbecf0c75baabf8f25e043b10999f46ec89c5aa1
Uxbel <beldetes@gmail.com>
2021-10-06 08:59:33 +0000

    Carga componentes usuario sin loguear

--- src/front/js/pages/usuarioHomeSinLoguear.js
+++ src/front/js/pages/usuarioHomeSinLoguear.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { BuscadorYFiltros } from "../component/buscadorYFiltros";
 import { CardResumenOferta } from "../component/cardResumenOferta";
 import "../../styles/home.scss";
 
@@ -9,12 +10,14 @@ export const HomeUsuarioSinLoguear = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col text-center mt-4">
+				<div className="col-8 text-center mt-4">
 					<h1>Home Usuario Sin Loguear</h1>
+					<BuscadorYFiltros />
 					<p>Columna para el componente buscador y el componente cards de las ofertas de trabajo</p>
 					<CardResumenOferta />
+					<CardResumenOferta />
 				</div>
-				<div className="col text-center mt-4">
+				<div className="col-4 text-center mt-4">
 					<p>Sidebar</p>
 				</div>
 			</div>
