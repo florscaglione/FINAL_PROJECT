d49d5c20dd7cc9a3ed0ec200584144643d9f1d07
florscaglione <florencia.scaglione@hotmail.com>
2021-10-08 16:36:02 +0000

    encrypted.py

--- src/api/encrypted.py
+++ src/api/encrypted.py
@@ -1,9 +1,11 @@
 import bcrypt
 
+# función de encriptado
 def encrypted_pass(password):
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
     return hashed
 
+# función de desencriptado
 def check_password_hash(password, encrypted_password):
     if (bcrypt.checkpw(password.encode('utf-8'), encrypted_password.encode('utf-8'))):
         print("It Matches!")
