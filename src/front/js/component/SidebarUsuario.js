a01b4ec6e51a474e0e27c3b723731b1ad25c1e8b
SMUTIS <sandramutissanabria@gmail.com>
2021-10-20 17:23:39 +0000

    sidebar usuario

---
+++ src/front/js/component/SidebarUsuario.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+export const SidebarUsuario = () => {
+	//const isLoggedIn = state.store.isLoggedIn;
+
+	return (
+		<div className="jumbotron jumbotron-fluid rounded">
+			<div className="container">
+				<h4 className="display-4">Trabaja para vivir</h4>
+				<p className="lead">Encuentra el trabajo en remoto que mejor se ajuste a tus necesidades.</p>
+			</div>
+		</div>
+	);
+};
--- src/front/js/pages/usuarioHomeSinLoguear.js
+++ src/front/js/pages/usuarioHomeSinLoguear.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import { BuscadorYFiltros } from "../component/buscadorYFiltros";
 import { CardResumenOferta } from "../component/cardResumenOferta";
 import "../../styles/home.scss";
+import { SidebarUsuario } from "../component/SidebarUsuario";
 
 export const HomeUsuarioSinLoguear = () => {
 	const { store, actions } = useContext(Context);
@@ -10,15 +11,15 @@ export const HomeUsuarioSinLoguear = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-8 text-center mt-4">
+				<div className="col-9 text-center mt-4">
 					<h1>Home Usuario Sin Loguear</h1>
 					<BuscadorYFiltros />
 					<p>Columna para el componente buscador y el componente cards de las ofertas de trabajo</p>
 					<CardResumenOferta />
 					<CardResumenOferta />
 				</div>
-				<div className="col-4 text-center mt-4">
-					<p>Sidebar</p>
+				<div className="col-3 text-center mt-4">
+					<SidebarUsuario />
 				</div>
 			</div>
 		</div>
