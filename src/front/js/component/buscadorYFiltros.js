446e2a36852b3b60dbe06f812154a77efe86a3f2
Uxbel <beldetes@gmail.com>
2021-11-01 18:01:26 +0000

    actualización

--- src/front/js/component/buscadorYFiltros.js
+++ src/front/js/component/buscadorYFiltros.js
@@ -1,6 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
 export const BuscadorYFiltros = () => {
+	const [remotes, setRemotes] = useState();
+	const [contracts, setContracts] = useState();
+	const [salaries, setSalaries] = useState();
+
+	/* useEffect(() => {
+		loadRemotes();
+		loadContracts();
+		loadSalaries();
+	}, []); */
+
+	/* async function loadRemotes() {
+		const url = `${process.env.BACKEND_URL}api/user-info/${id}/get`; // Revisar url con Flor
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		const data = await response.json();
+		setRemotes(data);
+	}
+
+	async function loadContracts() {
+		const url = `${process.env.BACKEND_URL}api/user-info/${id}/get`; // Revisar url con Flor
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		const data = await response.json();
+		setContracts(data);
+	}
+
+	async function loadSalaries() {
+		const url = `${process.env.BACKEND_URL}api/user-info/${id}/get`; // Revisar url con Flor
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		const data = await response.json();
+		setSalaries(data);
+	} */
+
 	return (
 		<div className="container">
 			<form className="d-flex">
@@ -10,20 +56,25 @@ export const BuscadorYFiltros = () => {
 					placeholder="Localizar ofertas"
 					aria-label="Search"
 				/>
-				<button className="btn btn-outline" type="submit">
+				<button className="" type="submit">
 					Buscar
 				</button>
 			</form>
 			<div className="m-1 d-flex flex-row-reverse bd-highlight">
-				<button className="btn btn-primary m-1 btn-sm" type="submit">
-					Filtro 1
-				</button>
-				<button className="btn btn-primary m-1 btn-sm" type="submit">
-					Filtro 2
-				</button>
-				<button className="btn btn-primary m-1 btn-sm" type="submit">
-					Filtro 3
-				</button>
+				<select className="form-select form-select-sm" aria-label="Default select example" type="submit">
+					<option selected>Trabajo Remoto</option>
+					{/* {remotes.map(remote => (
+						<option key={index}> {remote} </option>
+					))} */}
+				</select>
+				<select className="form-select form-select-sm" aria-label="Default select example" type="submit">
+					<option selected>Tipo Contrato</option>
+					{}
+				</select>
+				<select className="form-select form-select-sm" aria-label="Default select example" type="submit">
+					<option selected>Rango Salarial</option>
+					{}
+				</select>
 			</div>
 		</div>
 	);
